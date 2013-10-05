4060d105f8315cb80700ad5fda133c4edbfe396c
mooz <stillpedant@gmail.com>
2013-10-05 13:59:54 +0900

    Modify indentation in setup.py

--- setup.py
+++ setup.py
@@ -7,16 +7,17 @@ sys.path.append("test")
 
 import cmigemo
 
-setup(name             = "cmigemo",
-      version          = cmigemo.__version__,
-      author           = "Masafumi Oyamada",
-      author_email     = "stillpedant@gmail.com",
-      url              = "https://github.com/mooz/python-cmigemo",
-      description      = "A pure python binding for C/Migemo",
-      long_description = cmigemo.__doc__,
-      test_suite       = "test_cmigemo",
-      packages         = ["cmigemo"],
-      classifiers      = ["Operating System :: POSIX"],
-      keywords         = "C/Migemo ctypes",
-      license          = "MIT",
-      )
+setup(
+    name             = "cmigemo",
+    version          = cmigemo.__version__,
+    author           = "Masafumi Oyamada",
+    author_email     = "stillpedant@gmail.com",
+    url              = "https://github.com/mooz/python-cmigemo",
+    description      = "A pure python binding for C/Migemo",
+    long_description = cmigemo.__doc__,
+    test_suite       = "test_cmigemo",
+    packages         = ["cmigemo"],
+    classifiers      = ["Operating System :: POSIX"],
+    keywords         = "C/Migemo ctypes",
+    license          = "MIT",
+    )
